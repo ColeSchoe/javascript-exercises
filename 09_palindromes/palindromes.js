867daf9e744c1373cea8d31545903baf15298e92
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2024-05-30 21:58:05 -0500

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase();
+    newString = newString.replace(/[^\w\']|_/g, "");
+    // replaces anything that isn't a character with an empty string
 
+    if (newString.length <= 1) {
+        return true;
+    }
+    else if (newString.at(0) === newString.at(newString.length-1)) {
+        return palindromes(newString.slice(1, newString.length-1));
+    }
+    return false;
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
