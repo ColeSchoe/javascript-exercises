b159f7ed5734de72cd6299787e9f38c663dbf1f9
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2024-05-30 21:41:05 -0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((currentSum, currentNumber) => {
+    return currentSum + currentNumber;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((currentTotal, currentNumber) => {
+    return currentTotal * currentNumber;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  while (num > 0) {
+    sum *= num;
+    num--;
+  }
+  return sum;
 };
 
 // Do not edit below this line
