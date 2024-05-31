a41def6f483e08ea0d8472dc1b748ca9a2b8db66
Cole Schoenbauer <92380806+ColeSchoe@users.noreply.github.com>
2024-05-30 22:08:02 -0500

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(userInput) {
+    const number = parseInt(userInput);
+    if (number < 0) {
+        return "OOPS";
+    }
+    
+    let fibonacci_array = [0,1];
+    while (fibonacci_array.length <= number) {
+        fibonacci_array.push(fibonacci_array[fibonacci_array.length-1] + fibonacci_array[fibonacci_array.length-2]);
+    }
+    return fibonacci_array[number];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
